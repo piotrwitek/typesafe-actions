89b37cb65b0e1fc1e8e9a033a080a9d154a6fcc4
Piotr Witek <piotrek.witek@gmail.com>
2017-11-28 09:13:08 +0100

    added handling of edge cases in getType

--- src/get-type.ts
+++ src/get-type.ts
@@ -3,5 +3,9 @@ export interface TypeGetter<T> { getType?: () => T }
 export function getType<T extends string>(
   f: ((...args: any[]) => { type: T }) & TypeGetter<T>,
 ): T {
-  return f.getType!();
+  if (f == null || f.getType == null) {
+    throw new Error('first argument is not a "ts-action-creator" instance');
+  }
+
+  return f.getType();
 }
