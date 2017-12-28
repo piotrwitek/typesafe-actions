5fe3210e3de63c833ed3d3477f307d31fe9c5e9c
Piotr Witek <piotrek.witek@gmail.com>
2017-12-28 16:24:34 +0100

    improved errors consistency

--- src/get-type.ts
+++ src/get-type.ts
@@ -3,8 +3,12 @@ export interface TypeGetter<T> { getType?: () => T }
 export function getType<T extends string>(
   actionCreator: ((...args: any[]) => { type: T }) & TypeGetter<T>,
 ): T {
-  if (actionCreator == null || actionCreator.getType == null) {
-    throw new Error('first argument is not a "ts-action-creator" instance');
+  if (actionCreator == null) {
+    throw new Error('first argument is missing');
+  }
+
+  if (actionCreator.getType == null) {
+    throw new Error('first argument is not an instance of "typesafe-actions"');
   }
 
   return actionCreator.getType();
