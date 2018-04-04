2469f89d44af4acd4798eafd7b730b756bc29de7
Piotr Witek <piotrek.witek@gmail.com>
2018-04-04 20:03:18 +0200

    reformat isactionof

--- src/is-action-of.ts
+++ src/is-action-of.ts
@@ -1,7 +1,5 @@
 // tslint:disable:max-line-length
-import {
-  TypeMeta,
-} from './';
+import { TypeMeta } from './';
 
 export type AC<T extends { type: string }> = ((...args: any[]) => T) & TypeMeta<T['type']>;
 export type ACs<
@@ -9,31 +7,51 @@ export type ACs<
   T2 extends { type: string } = any,
   T3 extends { type: string } = any,
   T4 extends { type: string } = any,
-  T5 extends { type: string } = any,
-  > = (
-    |[AC<T1>]
-    | [AC<T1>, AC<T2>]
-    | [AC<T1>, AC<T2>, AC<T3>]
-    | [AC<T1>, AC<T2>, AC<T3>, AC<T4>]
-    | [AC<T1>, AC<T2>, AC<T3>, AC<T4>, AC<T5>]
-  );
+  T5 extends { type: string } = any
+> =
+  | [AC<T1>]
+  | [AC<T1>, AC<T2>]
+  | [AC<T1>, AC<T2>, AC<T3>]
+  | [AC<T1>, AC<T2>, AC<T3>, AC<T4>]
+  | [AC<T1>, AC<T2>, AC<T3>, AC<T4>, AC<T5>];
 
 /**
  * @description create the assert function that will assert a given action of `any` type to the specific "action type" if matching the specified action creator(s)
  */
 export function isActionOf<A extends { type: string }, T1 extends A>(
-  actionOrActions: AC<T1>
+  actionOrActions: AC<T1>,
 ): (action: A) => action is T1;
 
 /**
  * @description create the assert function that will assert a given action of `any` type to the specific "action type" if matching the specified action creator(s)
  */
-export function isActionOf<A extends { type: string }, T1 extends A, T2 extends A, T3 extends A, T4 extends A, T5 extends A>(
-  actionOrActions: ACs<T1> | ACs<T1, T2> | ACs<T1, T2, T3> | ACs<T1, T2, T3, T4> | ACs<T1, T2, T3, T4, T5>
+export function isActionOf<
+  A extends { type: string },
+  T1 extends A,
+  T2 extends A,
+  T3 extends A,
+  T4 extends A,
+  T5 extends A
+>(
+  actionOrActions:
+    | ACs<T1>
+    | ACs<T1, T2>
+    | ACs<T1, T2, T3>
+    | ACs<T1, T2, T3, T4>
+    | ACs<T1, T2, T3, T4, T5>,
 ): (action: A) => action is [T1, T2, T3, T4, T5][number];
 
-export function isActionOf<A extends { type: string }, T1 extends A, T2 extends A, T3 extends A, T4 extends A, T5 extends A>(
-  actionOrActions: AC<T1> | (ACs<T1> | ACs<T1, T2> | ACs<T1, T2, T3> | ACs<T1, T2, T3, T4> | ACs<T1, T2, T3, T4, T5>)
+export function isActionOf<
+  A extends { type: string },
+  T1 extends A,
+  T2 extends A,
+  T3 extends A,
+  T4 extends A,
+  T5 extends A
+>(
+  actionOrActions:
+    | AC<T1>
+    | (ACs<T1> | ACs<T1, T2> | ACs<T1, T2, T3> | ACs<T1, T2, T3, T4> | ACs<T1, T2, T3, T4, T5>),
 ) {
   return (action: A): action is [T1, T2, T3, T4, T5][number] => {
     if (actionOrActions == null) {
@@ -47,7 +65,8 @@ export function isActionOf<A extends { type: string }, T1 extends A, T2 extends 
     }
 
     const actionCreators: any[] = Array.isArray(actionOrActions)
-      ? actionOrActions : [actionOrActions];
+      ? actionOrActions
+      : [actionOrActions];
 
     return actionCreators.some((actionCreator, index) => {
       if (actionCreator.getType == null) {
