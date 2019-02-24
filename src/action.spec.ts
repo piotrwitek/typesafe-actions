c4db0f20ee2dd3118aafe5dd681b66b46f36fe57
Steven Syrek <sjsyrek@users.noreply.github.com>
2019-02-24 22:28:22 +0100

    Add error property to action (#119)
    Thank you for your contribution! :thumbsup:
    
    Please makes sure that these checkboxes are checked before submitting your PR, thank you!
    
    Related issues: https://github.com/piotrwitek/typesafe-actions/issues/116
    
    * [x] PR have a corresponding issue which is approved and it's linked above
    * [x] Rebase before creating a PR to keep commit history clean
    * [x] Clear node_modules and reinstall all the dependencies: `npm run reinstall`
    * [x] Run ci-check npm script: `npm run ci-check`
    
    For bugfixes:
     * [x] Add at least one unit test to cover the bug that have been fixed
    
    For new feature:
     * [ ] Update API docs and tutorial
     * [ ] Add examples to demonstrate new feature
     * [x] Add type unit tests
     * [x] Add runtime unit tests

--- src/action.spec.ts
+++ src/action.spec.ts
@@ -1,4 +1,4 @@
-import * as T from './type-helpers';
+import * as T from './type-helpers'; // type-tests global
 import { action } from './action';
 
 it.skip('skip', () => undefined);
@@ -81,4 +81,58 @@ describe('with optional payload and optional meta', () => {
   withOptionalPayloadAndOptionalMeta('Hello!'); // => { type: 'ACTION', payload: 'Hello!' }
   // @dts-jest:pass:snap
   withOptionalPayloadAndOptionalMeta('Hello!', 'info'); // => { type: 'ACTION', payload: 'Hello!', meta: 'info' }
+});
+
+describe('with error', () => {
+  const withError = (error: boolean) =>
+    action('ACTION', undefined, undefined, error);
+  // @dts-jest:pass:snap
+  withError(true); // => { type: 'ACTION', error: true }
+});
+
+describe('with optional error', () => {
+  const withOptionalError = (error?: boolean) =>
+    action('ACTION', undefined, undefined, error);
+  // @dts-jest:pass:snap
+  withOptionalError(); // => { type: 'ACTION' }
+  // @dts-jest:pass:snap
+  withOptionalError(true); // => { type: 'ACTION', error: true }
+});
+
+describe('with payload, meta, and error', () => {
+  const withPayloadMetaAndError = (
+    message: string,
+    scope: string,
+    error: boolean
+  ) => action('ACTION', message, scope, error);
+  // @dts-jest:pass:snap
+  withPayloadMetaAndError('Hello!', 'info', true); // => { type: 'ACTION', payload: 'Hello!', meta: 'info', error: true }
+});
+
+describe('with payload, meta, and optional error', () => {
+  const withPayloadMetaAndOptionalError = (
+    message: string,
+    scope: string,
+    error?: boolean
+  ) => action('ACTION', message, scope, error);
+  // @dts-jest:pass:snap
+  withPayloadMetaAndOptionalError('Hello!', 'info'); // => { type: 'ACTION', payload: 'Hello!', meta: 'info' }
+  // @dts-jest:pass:snap
+  withPayloadMetaAndOptionalError('Hello!', 'info', true); // => { type: 'ACTION', payload: 'Hello!', meta: 'info', error: true }
+});
+
+describe('with optional payload, optional meta, and optional error', () => {
+  const withOptionalPayloadOptionalMetaAndOptionalError = (
+    message?: string,
+    scope?: string,
+    error?: boolean
+  ) => action('ACTION', message, scope, error);
+  // @dts-jest:pass:snap
+  withOptionalPayloadOptionalMetaAndOptionalError(); // => { type: 'ACTION' }
+  // @dts-jest:pass:snap
+  withOptionalPayloadOptionalMetaAndOptionalError('Hello!'); // => { type: 'ACTION', payload: 'Hello!' }
+  // @dts-jest:pass:snap
+  withOptionalPayloadOptionalMetaAndOptionalError('Hello!', 'info'); // => { type: 'ACTION', payload: 'Hello!', meta: 'info' }
+  // @dts-jest:pass:snap
+  withOptionalPayloadOptionalMetaAndOptionalError('Hello!', 'info', true); // => { type: 'ACTION', payload: 'Hello!', meta: 'info', error: true }
 });
