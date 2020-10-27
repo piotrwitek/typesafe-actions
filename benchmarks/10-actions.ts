cbed746b3e997942cc93c3b4fa5abb876194ee16
Piotr Witek <piotrek.witek@gmail.com>
2020-10-27 09:11:15 +0100

    Updated benchmarks

--- benchmarks/10-actions.ts
+++ benchmarks/10-actions.ts
@@ -1,16 +1,16 @@
 import { ActionType, createAction, createReducer } from '../src';
 
 export const actions = {
-  a1: createAction('a1'),
-  a2: createAction('a2'),
-  a3: createAction('a3'),
-  a4: createAction('a4'),
-  a5: createAction('a5'),
-  a6: createAction('a6'),
-  a7: createAction('a7'),
-  a8: createAction('a8'),
-  a9: createAction('a9'),
-  a10: createAction('a10'),
+  a1: createAction('a1')(),
+  a2: createAction('a2')(),
+  a3: createAction('a3')(),
+  a4: createAction('a4')(),
+  a5: createAction('a5')(),
+  a6: createAction('a6')(),
+  a7: createAction('a7')(),
+  a8: createAction('a8')(),
+  a9: createAction('a9')(),
+  a10: createAction('a10')(),
 };
 
 export type RootAction = ActionType<typeof actions>;
